d721a9740c1b3847240da856bf8177f955944297
jwashaba <166251049+jwashaba@users.noreply.github.com>
2025-08-26 18:56:31 -0700

    simple enemy and dashing

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public SpriteRenderer enemySR;
+    public Rigidbody2D enemyRB;
+    public float moveSpeed;
+    public float patrolDist;
+    public float idleTime;
+    private Vector3 startingPosition;
+    private int direction = 1;
+    private float waitTimer = 0f;
+    private bool isWaiting = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWaiting)
+        {
+            waitTimer += Time.deltaTime;
+            if (waitTimer >= idleTime)
+            {
+                float clampedX = startingPosition.x + direction * patrolDist;
+                transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+                isWaiting = false;
+                waitTimer = 0f;
+                direction *= -1;
+                enemySR.flipX = !enemySR.flipX;
+            }
+            return;
+        }
+
+        transform.Translate(Vector3.right * direction * moveSpeed * Time.deltaTime);
+
+        if (Mathf.Abs(transform.position.x - startingPosition.x) >= patrolDist) {
+            isWaiting = true;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D playerRB;
     public SpriteRenderer playerSR;
-    public float speed;
+    public Rigidbody2D playerRB;
+    public float moveSpeed;
+    private Vector2 moveInput = Vector2.zero;
+    public float dashSpeed;
+    public float dashTime;
+    public float dashCooldown;
+    private bool canDash = true;
+    private bool isDashing = false;
+    private Vector2 dashDir = Vector2.down;
 
     // TEMP ACCESS TO PLAYER CAMERA & FLASHLIGHT -- REMOVE ON MERGE
     public Camera _cam;
@@ -20,41 +28,72 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         // TEMP FLASHLIGHT MOVEMENT FOR PLAYER -- REMOVE ON MERGE
-        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 playerPos = playerRB.position;
-        Vector2 dir = (mouseWorld - playerPos); 
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        _light.angle = angle + _light.fov / 2f;
+        // Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Vector3 playerPos = playerRB.position;
+        // Vector2 dir = (mouseWorld - playerPos); 
+        // float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        // _light.angle = angle + _light.fov / 2f;
+
+        float x = 0f, y = 0f;
+
+        if (!isDashing)
+        {
+            //horizontal movement
+            if (Input.GetKey(KeyCode.A))
+            {
+                x = -1f;
+                playerSR.flipX = false;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                x = 1f;
+                playerSR.flipX = true;
+            }
+
+            // vertical movement
+            if (Input.GetKey(KeyCode.W))
+            {
+                y = 1f;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                y = -1f;
+            }
+
+            moveInput = new Vector2(x, y).normalized;
+
+            if (moveInput != Vector2.zero)
+            {
+                dashDir = moveInput;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && canDash)
+            {
+                StartCoroutine(DashTiming());
+            }
+        }
     }
 
     // physics loop for movement
     void FixedUpdate()
     {
-        float x = 0f, y = 0f;
+        if (isDashing)
+        {
+            playerRB.linearVelocity = dashDir * dashSpeed;
+        }
+        else
+        {
+            playerRB.linearVelocity = moveInput * moveSpeed;
+        }
+    }
 
-        //horizontal movement
-        if (Input.GetKey(KeyCode.A))
-        {
-            x = -1f;
-            playerSR.flipX = false;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            x = 1f;
-            playerSR.flipX = true;
-        }
-
-        // vertical movement
-        if (Input.GetKey(KeyCode.W))
-        {
-            y = 1f;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            y = -1f;
-        }
-
-        Vector2 input = new Vector2(x, y).normalized;
-        playerRB.linearVelocity = input * speed;
+    IEnumerator DashTiming()
+    {
+        canDash = false;
+        isDashing = true;
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
