d6b20faf1c85e3eee9a84c00ec2ab6da31da2009
jwashaba <166251049+jwashaba@users.noreply.github.com>
2025-08-26 23:22:28 -0700

    working on enemy movement points

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using System;
 
 public class EnemyMovement : MonoBehaviour
 {
     public SpriteRenderer enemySR;
     public Rigidbody2D enemyRB;
     public float moveSpeed;
-    public float patrolDist;
     public float idleTime;
-    private Vector3 startingPosition;
+    public bool looping;
+    public Transform[] points;
+
+    public float threshold;
+    private int currPoint = 0;
     private int direction = 1;
     private float waitTimer = 0f;
     private bool isWaiting = false;
@@ -15,31 +19,50 @@ public class EnemyMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        startingPosition = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isWaiting)
+        if (isWaiting && points.Length > 0)
         {
+            enemyRB.linearVelocity = Vector2.zero;
+
             waitTimer += Time.deltaTime;
             if (waitTimer >= idleTime)
             {
-                float clampedX = startingPosition.x + direction * patrolDist;
-                transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
                 isWaiting = false;
                 waitTimer = 0f;
-                direction *= -1;
-                enemySR.flipX = !enemySR.flipX;
+                if (looping)
+                {
+                    currPoint = (currPoint + 1) % points.Length;
+                }
+                else // go back across path
+                {
+                    if (currPoint == 0) direction = 1;
+                    else if (currPoint == points.Length - 1) direction = -1;
+                    currPoint += direction;
+                }
             }
             return;
         }
 
-        transform.Translate(Vector3.right * direction * moveSpeed * Time.deltaTime);
-
-        if (Mathf.Abs(transform.position.x - startingPosition.x) >= patrolDist) {
+        Vector3 nextPoint = points[currPoint].position;
+        Vector3 diff = nextPoint - transform.position;
+        float maxStep = moveSpeed * Time.deltaTime;
+        float dist = diff.magnitude;
+        if (dist <= Math.Max(maxStep, threshold))
+        {
+            transform.position = nextPoint;
+            enemyRB.linearVelocity = Vector2.zero;
             isWaiting = true;
+            return;
         }
+
+        Vector3 stepLength = diff.normalized * maxStep;
+        // if (stepLength.sqrMagnitude > dist.sqrMagnitude) stepLength = dist;
+        transform.position += stepLength;
+        // need to flip sprite?
     }
 }
